b5ba80cc1124cbe9b0ce4afe4f3e5b34d0245c54
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-09-27 21:36:01 +0530

    fixed for test

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,26 @@ import Home from "../src/Pages/Home";
 import Login from "../src/Pages/Login";
 import Game from "../src/Pages/Game";
 
-const ProtectedRoute = ({ element }) => {
-  const { isAuthenticated } = useAuthStore();
-  return isAuthenticated ? element : <Navigate to="/login" />;
-};
+// const ProtectedRoute = ({ element }) => {
+//   const { isAuthenticated } = useAuthStore();
+//   return isAuthenticated ? element : <Navigate to="/login" />;
+// };
 
 const App = () => {
   return (
     <main className="min-h-screen w-full flex bg-[#000] text-white overflow-hidden">
-      <Routes>
+      {/* <Routes>
         <Route path="/login/:gameId" element={<Login />} />
         <Route path="/" element={<ProtectedRoute element={<Home />} />} />
         <Route
           path="/game/:gameId"
           element={<ProtectedRoute element={<Game />} />}
         />
+      </Routes> */}
+      <Routes>
+        <Route path="/login/:gameId" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/game/:gameId" element={<Game />} />
       </Routes>
     </main>
   );
