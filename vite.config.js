aa18925c26672ec0e79b5352da4d3b46cdecbcf5
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-09-29 10:07:38 +0530

    done again

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,26 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://3.109.212.132:8080",
+      changeOrigin: true,
+      secure: false,
     },
   },
 });
+
+// vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/user': {
+//         target: 'https://3.109.212.132:8080', // Use HTTPS to avoid mixed content
+//         changeOrigin: true,
+//         secure: false, // If the target uses a self-signed certificate
+//       },
+//     },
+//   },
+// });
