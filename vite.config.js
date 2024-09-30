58f290afc4884437997239bbfef7535334070083
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-09-30 14:38:11 +0530

    fixed in dev env

--- vite.config.js
+++ vite.config.js
@@ -6,26 +6,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "https://3.109.212.132:8080",
-      changeOrigin: true,
-      secure: false,
+      // "/api": {
+      //   target: "http://3.109.212.132:8080", // Correctly structured proxy object
+      //   changeOrigin: true,
+      //   secure: false,
+      // },
+      "/user": {
+        target: "https://3.109.212.132:8080", // Use HTTPS to avoid mixed content
+        changeOrigin: true,
+        secure: false, // If the target uses a self-signed certificate
+      },
     },
   },
 });
-
-// vite.config.js
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/user': {
-//         target: 'https://3.109.212.132:8080', // Use HTTPS to avoid mixed content
-//         changeOrigin: true,
-//         secure: false, // If the target uses a self-signed certificate
-//       },
-//     },
-//   },
-// });
