5b1dce901abd7b966cafabc275e4cb30e97e81d3
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-09-30 15:20:30 +0530

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,18 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // "/api": {
-      //   target: "http://3.109.212.132:8080", // Correctly structured proxy object
-      //   changeOrigin: true,
-      //   secure: false,
-      // },
-      "/user": {
-        target: "https://3.109.212.132:8080", // Use HTTPS to avoid mixed content
-        changeOrigin: true,
-        secure: false, // If the target uses a self-signed certificate
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     // "/api": {
+  //     //   target: "http://3.109.212.132:8080", // Correctly structured proxy object
+  //     //   changeOrigin: true,
+  //     //   secure: false,
+  //     // },
+  //     // "/user": {
+  //     //   target: "https://3.109.212.132:8080", // Use HTTPS to avoid mixed content
+  //     //   changeOrigin: true,
+  //     //   secure: false, // If the target uses a self-signed certificate
+  //     // },
+  //   },
+  // },
 });
