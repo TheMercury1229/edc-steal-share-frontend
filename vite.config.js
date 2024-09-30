2335abbeb760997551cbbddc06351de81764e83a
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-09-30 15:21:31 +0530

    Useless Commit Ignore

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,26 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // "/api": {
-      //   target: "http://3.109.212.132:8080", // Correctly structured proxy object
-      //   changeOrigin: true,
-      //   secure: false,
-      // },
-      "/user": {
-        target: "https://3.109.212.132:8080", // Use HTTPS to avoid mixed content
-        changeOrigin: true,
-        secure: false, // If the target uses a self-signed certificate
-      },
+      "/api": "https://3.109.212.132:8080",
+      changeOrigin: true,
+      secure: false,
     },
   },
 });
+
+// vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/user': {
+//         target: 'https://3.109.212.132:8080', // Use HTTPS to avoid mixed content
+//         changeOrigin: true,
+//         secure: false, // If the target uses a self-signed certificate
+//       },
+//     },
+//   },
+// });
